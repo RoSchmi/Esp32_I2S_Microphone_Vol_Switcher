9e4c7a0e9ac6603b4ddbb7f41f951c8dc561c58a
RoSchmi <Dr-Roland-Schmidt@t-online.de>
2024-06-29 17:33:03 +0200

    Changed code in soundswitcher.cpp from i2s_read_bytes to i2s_read

--- lib/RoSchmi/SensorData/SoundSwitcher.cpp
+++ lib/RoSchmi/SensorData/SoundSwitcher.cpp
@@ -232,7 +232,9 @@ void SoundSwitcher::SetActive()
 float SoundSwitcher::getSoundFromMicro()
 {   
     int32_t audio_buf[BUFLEN];
-    int bytes_read = i2s_read_bytes(i2s_num, audio_buf, sizeof(audio_buf), portMAX_DELAY);
+    size_t bytes_read;
+    i2s_read(i2s_num, audio_buf, sizeof(audio_buf), &bytes_read, portMAX_DELAY);
+    //int bytes_read = i2s_read_bytes(i2s_num, audio_buf, sizeof(audio_buf), portMAX_DELAY);
     int32_t cleanBuf[BUFLEN / 2] {0};
     int cleanBufIdx = 0;
     for (int i = 0; i < BUFLEN; i++)
